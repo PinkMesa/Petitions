ff1ed6d7aae9d99f589521510c22227ab5fe882e
SS <choloepusp@gmail.com>
2020-05-03 20:00:13 +0300

    Serialized methods from Petition model

--- petitions/serializers.py
+++ petitions/serializers.py
@@ -8,9 +8,17 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 class PetitionSerializer(serializers.ModelSerializer):
+    voting_passed = serializers.SerializerMethodField()
+    expired = serializers.SerializerMethodField()
     class Meta:
         model = Petition
         fields = ('__all__')
+    
+    def get_voting_passed(self, obj):
+        return obj.voting_passed
+
+    def get_expired(self, obj):
+        return obj.expired
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
