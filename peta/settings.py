6b91b0cabc3dce7bb74e1fda569ebdcf59c1f996
Vladyslav <lrazerz9ine@gmail.com>
2020-06-05 01:36:02 +0300

    asd

--- peta/settings.py
+++ peta/settings.py
@@ -186,7 +186,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Redirects
 LOGIN_REDIRECT_URL = '/'
