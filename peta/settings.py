f833a02ff8dfd239c14fa12dced795a1bd049a88
Vladyslav <lrazerz9ine@gmail.com>
2020-06-05 03:25:50 +0300

    asa

--- peta/settings.py
+++ peta/settings.py
@@ -193,6 +193,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/'
 
 
-import dj-database-url
-prod_db  =  dj-database-url.config(conn_max_age=500)
+import dj_database_url
+prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
